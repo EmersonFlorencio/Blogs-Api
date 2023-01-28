e5063a7932eed01019a19524e5bda1eaedd58190
Emerson Florencio <emersonflorencio@hotmail.com>
2023-01-28 18:24:56 -0300

    resolvendo o problema no req01

--- src/migrations/20230128192414-create-posts-categories.js
+++ src/migrations/20230128192414-create-posts-categories.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostCategory', {
+    await queryInterface.createTable('posts_categories', {
       post_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
